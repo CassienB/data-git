13d741e197708656ddfaa302ba40c8af46cd1d32
cassien babey <cassien98@yahoo.fr>
2022-11-18 09:52:07 +0100

    Implement my_name_is function

--- today.py
+++ today.py
@@ -3,4 +3,16 @@ This is the module docstring
 '''
 
 
-pass  # YOUR CODE HERE
+# $CHALLENGIFY_BEGIN
+def my_name_is():
+    '''
+    This is the function docstring
+    '''
+    return "ssaunier"
+
+def my_buddy_is():
+    '''
+    This is the function docstring
+    '''
+    return "ssaunier"
+# $CHALLENGIFY_END
